f80c5f00f770141aaa2dfc1d5d4fa25665a09489
Václav Chlumský <chlumskyvaclav@gmail.com>
2025-12-04 07:15:33 +0100

    2025 4-2

---
+++ 2025/4-2.py
@@ -0,0 +1,32 @@
+input_file = '4.input'
+lines = []
+
+with open(input_file, 'r') as f:
+    lines = f.readlines()
+
+grid = [[0 for x in range(len(lines[0]))] for y in range(len(lines))]
+for x in range(0, len(lines)):
+    line = lines[x].strip()
+    for y in range(0, len(line)):
+        grid[x][y] = line[y]
+
+def check(x,y):
+    if grid[x][y] != '@':
+        return 0
+    
+    positions = [1, 0, -1]
+    free = [x + i < 0 or x + i >= len(grid) or y + j < 0 or y + j>= len(grid[i]) or grid[x + i][y + j] != '@' for i in positions for j in positions]
+    return sum(free) > 4
+
+res = 0
+do = True
+while do:
+    do = False
+    for x in range(0, len(grid)):
+        for y in range(0, len(grid[x])):
+            if check(x, y):
+                grid[x][y] = '.'
+                res += 1
+                do = True
+
+print(res)
