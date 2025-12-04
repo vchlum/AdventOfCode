1acc70dc7c4b7320243e67540c76ee2680034875
Václav Chlumský <chlumskyvaclav@gmail.com>
2025-12-04 10:48:36 +0100

    2024 1-2

---
+++ 2024/1-2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int compare(const void* a1, const void* b1) {
+    int a = *((int*)a1);
+    int b = *((int*)b1);
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+}
+
+int main() {
+    int first, second, size;
+    int *first_arr = calloc(1, sizeof(int));
+    int *second_arr = calloc(1, sizeof(int));
+    int array_size = 1;
+    
+    FILE *f = fopen("1.input", "r");
+
+    size = 0;
+    while (fscanf(f, "%d   %d", &first, &second) == 2) {
+        size ++;
+        if (size > array_size) {
+            array_size *= 2;
+            first_arr = realloc(first_arr, array_size * sizeof(int));
+            second_arr = realloc(second_arr, array_size * sizeof(int));
+           
+        }
+        first_arr[size -1 ] = first;
+        second_arr[size -1] = second;
+    }
+        
+    fclose(f);
+
+    qsort(first_arr, size, sizeof(int), compare);
+    qsort(second_arr, size, sizeof(int), compare);
+
+    int res = 0;
+    int start = 0;
+    for (int i = 0; i < size; i++) {
+        int multiplicator = 0;
+        for (int j = start; j < size; j++) {
+            if (second_arr[j] > first_arr[i])
+                break;
+            if (first_arr[i] == second_arr[j])
+                multiplicator++;
+        }
+        res += first_arr[i] * multiplicator;
+    }
+    printf("%d\n", res);
+
+    return 0;
+}
