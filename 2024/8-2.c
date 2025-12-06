fbf6534955d6f3b44a94f226fe3061535795fd18
Václav Chlumský <chlumskyvaclav@gmail.com>
2025-12-06 18:38:24 +0100

    2024 8

---
+++ 2024/8-2.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+struct point {
+    char antenna;
+    char antinode;
+};
+
+int main() {
+    FILE *f = fopen("8.input", "r");
+    char *line = NULL;
+    size_t len = 0;
+    int read;
+    int res = 0;
+    int map_size = 1;
+    struct point **map = calloc(map_size, sizeof(struct point*));
+    int mapX, mapY = -1;
+    while ((read = getline(&line, &len, f)) != -1) {
+        mapY++;
+        if (map_size < mapY) {
+            map_size *= 2;
+            map = realloc(map, map_size * sizeof(struct point *));
+        }
+        mapX = strlen(line) -1;
+        map[mapY] = calloc(mapX, sizeof(struct point));
+        for (int i = 0; i < mapX; i ++) {
+            map[mapY][i].antenna = line[i];
+            map[mapY][i].antinode = '.';
+        }
+    }
+    if (line)
+        free(line);
+    mapY++; /* last line */
+    int x,y;
+    int dX, dY;
+    int distX, distY;
+    for (int i = 0; i < mapY; i++) {
+        for (int j = 0; j < mapX; j++) {
+            if (map[i][j].antenna != '.') {
+                for (int k = 0; k < mapY; k++) {
+                    for (int l = 0; l < mapX; l++) {
+                        if (map[i][j].antenna == map[k][l].antenna) {
+                            if (i == k && j == l) continue;
+                            distX = j - l;
+                            distY = i - k;
+                            dX = distX;
+                            dY = distY;
+                            while (k - dY >= 0 && k - dY < mapY && l - dX >= 0 && l - dX < mapX) {
+                                map[k - dY][l - dX].antinode = '#';
+                                dX -= distX;
+                                dY -= distY;
+                                map[i][j].antinode = '#';
+                                map[k][l].antinode = '#';
+                            }
+                            dX = distX * -1;
+                            dY = distY * -1;
+                            while (i + dY >= 0 && i + dY < mapY && j + dX >= 0 && j + dX < mapX) {
+                                map[i + dY][j + dX].antinode = '#';
+                                dX += distX * -1;
+                                dY += distY * -1;
+                                map[i][j].antinode = '#';
+                                map[k][l].antinode = '#';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    res = 0;
+    for (int i = 0; i < mapY; i++)
+        for (int j = 0; j < mapX; j++)
+            if (map[i][j].antinode == '#')
+                res++;
+    printf("%lld\n", res);
+    fclose(f);
+    return 0;
+}
