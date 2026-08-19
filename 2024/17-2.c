8d34e03ef73124489d030d0656cd7cbb0dea8798
Václav Chlumský <chlumskyvaclav@gmail.com>
2026-08-19 10:24:52 +0200

    2024 17

---
+++ 2024/17-2.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/* gcc 17-2.c -g -o 17-2 -lm */
+
+long int A, B, C;
+
+int *prog;
+int proglen = 0;
+int out[1000];
+int outlen = 0;
+
+int checkProg() {
+    int i;
+    for (i = proglen - 1; i >= 0; i--) {
+        if (out[i] != prog[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+long int getOperand(int operand) {
+    switch (operand) {
+        case 0:
+            return 0;
+        case 1:
+            return 1;
+        case 2:
+            return 2;
+        case 3:
+            return 3;
+        case 4:
+            return A;
+        case 5:
+            return B;
+        case 6:
+            return C;
+        default:
+            return -1;
+    }
+    return 0;
+}
+
+int main(void) {
+    FILE *f = fopen("17.input", "r");
+    char *line = NULL;
+    size_t len = 0;
+    int read;
+    int p;
+    int i;
+    while ((read = getline(&line, &len, f)) != -1) {
+        if (strlen(line) < 1)
+            continue;
+        if (strncmp(line, "Register", strlen("Register")) == 0) {
+        switch (strcmp(line, "Register ")) {
+            case 'A':
+                A = atoi(line + strlen("Register ") + 2);
+                break;
+            case 'B':
+                B = atoi(line + strlen("Register ") + 2);
+                break;
+            case 'C':
+                C = atoi(line + strlen("Register ") + 2);
+                break;
+        }
+        }
+        if (strncmp(line, "Program:", strlen("Program:")) == 0) {
+            char *progline = line + strlen("Program: ");
+            prog = malloc(sizeof(int) * 300);
+            char *token = strtok(progline, ",");
+            while (token != NULL) {
+                prog[proglen++] = atoi(token);
+                token = strtok(NULL, ",");
+            }
+        }
+    }
+
+    long int initA = 1;
+    do {
+        A = initA;
+        outlen = 0;
+        for (i = 0; i < proglen; ) {
+            int operand = prog[i + 1];
+            long int operandCombo = getOperand(operand);
+            int newI = 0;
+            switch (prog[i]) {
+                case 0:
+                    A = (long int) (A / ((long int) pow(2, operandCombo)));
+                    break;
+                case 1:
+                    B = B ^ operand;
+                    break;
+                case 2:
+                    B = operandCombo % 8;
+                    break;
+                case 3:
+                    if (A != 0) {
+                        i = operand;
+                        newI = 1;
+                    }
+                    break;
+                case 4:
+                    B = B ^ C;
+                    break;
+                case 5:
+                    out[outlen++] = operandCombo % 8;
+                    break;
+                case 6:
+                    B = (long int) (A / ((long int) pow(2, operandCombo)));
+                    break;
+                case 7:
+                    C = (long int) (A / ((long  int) pow(2, operandCombo)));
+                    break;
+            }
+            if (!newI) {
+                i += 2;
+            }
+        }
+        if (outlen < proglen) {
+            initA *= 2;
+            continue;
+        }
+        i = checkProg();
+        if (i > -1) {
+            initA += pow(8, i); // i-th
+        }
+    } while (checkProg() > -1);
+    printf("A: %ld\n", initA);
+
+    fclose(f);
+    free(line);
+    free(prog);
+    return 0;
+}
